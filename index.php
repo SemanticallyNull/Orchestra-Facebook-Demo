d8ade9b990f9b3281918d341a5bec132f2c2b4b1
Ben Chapman <ben@dismiz.com>
2012-03-20 14:52:44 -0700

    Made logo smaller and added margin at bottom of header

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if($facebook->getUser() !== 0) { // If the user is logged in
 	<body>
 		<div id="outer_container">
 			<div id="header">
-				<img src="assets/orchestra.png" title="Orchestra" alt="Orchestra">
+				<img id="logo" src="assets/orchestra.png" title="Orchestra" alt="Orchestra">
 			</div>
 			<h1>Hi <?=$user_data['name']?></h1>
 		</div>
