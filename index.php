b28531b3ba37494e1da6dbc2a1fec705fd4ba983
Ben Chapman <ben@dismiz.com>
2012-03-20 16:59:34 -0700

    Javascript... need better implementation

--- index.php
+++ index.php
@@ -18,10 +18,10 @@ if($facebook->getUser() != 0) { // If the user is logged in
 	} catch(FacebookApiException $e) {
 		// Facebook may give you a user ID even if the user is logged
 		// out. You'll need to make them login again.
-		header("Location: " . $facebook->getLoginUrl() ); // Redirect the user to the login page.
+		echo "<script type='text/javascript'>top.location.href = '".$facebook->getLoginUrl()."';</script>"; // Redirect the user to the login page.
 	}
 } else { // If the user is not logged in
-	header("Location: " . $facebook->getLoginUrl() ); // Redirect the user to the login page.
+	echo "<script type='text/javascript'>top.location.href = '".$facebook->getLoginUrl()."';</script>"; // Redirect the user to the login page.
 }
 
 ?>
