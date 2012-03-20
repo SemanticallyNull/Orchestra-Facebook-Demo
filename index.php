76657b179a1f7dc46a004e1e74a1ab378268ca92
Ben Chapman <ben@dismiz.com>
2012-03-20 14:35:29 -0700

    Changed CSS font stack

--- index.php
+++ index.php
@@ -32,6 +32,9 @@ if($facebook->getUser() !== 0) { // If the user is logged in
 	</head>
 	<body>
 		<div id="outer_container">
+			<div id="header">
+				<img src="https://my.orchestra.io/images/template_logo.png" title="Orchestra" alt="Orchestra">
+			</div>
 			<h1>Hi <?=$user_data['name']?></h1>
 		</div>
 	</body>
