39028562a3e66d177f7e25c737868d3498b28342
Ben Chapman <ben@dismiz.com>
2013-02-16 01:18:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require_once("lib/facebook.php");
 
 // You need to configure these. Get info from: https://developers.facebook.com/apps/
 $fb_config = array(
-					'appId' => '179983022118900',
-					'secret' => '2e9bbd94722e7cd9a2886e326437efc9',
+					'appId' => 'x',
+					'secret' => 'x',
 					// On Orchestra fileUpload will almost always be false. 
 					// See http://docs.orchestra.io/kb/system-constraints/system-constraints#file-uploads-hosting
 					'fileUpload' => false, 
